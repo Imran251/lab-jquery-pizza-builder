c45ea55ddb4cbbd51ee0bb37c621ccdfe154c250
Imran Syed <syed.imran2512@gmail.com>
2017-10-26 21:13:09 -0400

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,34 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready( function () {
+ $('.btn-pepperonni').click( function () {
+   $('.pep').toggle();
+   $('.btn-pepperonni').toggleClass('active');
+   $(".panel.price ul li:nth-child(1)").toggle();
+   $('.btn-pepperonni').hasClass("active") ? realPrice += 1 : realPrice -= 1;
+
+ });
+ $('.btn-mushrooms').click( function () {
+   $('.mushroom').toggle();
+   $('.btn-mushrooms').toggleClass('active');
+   $(".panel.price ul li:nth-child(2)").toggle();
+ });
+
+ $('.btn-green-peppers').click( function () {
+   $('.green-pepper').toggle();
+   $('.btn-green-peppers').toggleClass('active');
+   $(".panel.price ul li:nth-child(3)").toggle();
+ });
+
+ $('.sauce').removeClass('sauce-white');
+ $('.btn-sauce').click( function () {
+   $('.sauce').toggleClass('sauce-white');
+   $('.btn-sauce').toggleClass('active');
+   $(".panel.price ul li:nth-child(4)").toggle();
+ });
+ $('.crust').removeClass('crust-gluten-free');
+ $('.btn-crust').click( function () {
+   $('.crust').toggleClass('crust-gluten-free');
+   $('.btn-crust').toggleClass('active');
+   $(".panel.price ul li:nth-child(5)").toggle();
+ });
+
+});
